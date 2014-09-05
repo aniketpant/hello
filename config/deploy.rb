b266624f63a22e35d36851c9f66711dfe5a59a7a
Aniket Pant <me@aniketpant.com>
2014-09-06 00:06:21 +0530

    Add deploy script

---
+++ config/deploy.rb
@@ -0,0 +1,33 @@
+require './config/settings.rb'
+
+require 'mina/bundler'
+require 'mina/rails'
+require 'mina/git'
+require 'mina/rbenv'
+
+set :shared_paths, ['log']
+
+task :environment do
+  invoke :'rbenv:load'
+end
+
+task :setup => :environment do
+  queue! %[mkdir -p "#{deploy_to}/shared/log"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/log"]
+
+  queue! %[touch "#{deploy_to}/shared/rack.pid"]
+end
+
+desc "Deploys the current version to the server."
+task :deploy => :environment do
+  deploy do
+    invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
+    invoke :'bundle:install'
+
+    to :launch do
+      queue "if [ -f rack.pid ]; then kill -9 `cat rack.pid`; else print 'no server running'; fi"
+      queue "rackup -D -p 4567 -s thin -P rack.pid config.ru"
+    end
+  end
+end
